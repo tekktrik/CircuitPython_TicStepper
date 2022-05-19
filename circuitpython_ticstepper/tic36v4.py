f57054823bfc875297599c09a97107ae50e94fe3
Alec Delaney <tekktrik@gmail.com>
2022-05-19 15:58:31 -0400

    From typing block (not needed), reformatted

--- circuitpython_ticstepper/tic36v4.py
+++ circuitpython_ticstepper/tic36v4.py
@@ -16,25 +16,17 @@ TIC Motor over I2C control
 from circuitpython_ticstepper.i2c import TicMotorI2C
 from circuitpython_ticstepper.constants import StepMode
 
-try:
-    import typing  # pylint: disable=unused-import
-    from busio import I2C
-    from circuitpython_ticstepper.constants import (  # pylint: disable=ungrouped-imports
-        StepModeValues,
-    )
-except ImportError:
-    pass
 
 class TIC36v4_I2C(TicMotorI2C):
 
     MAX_CURRENT_LIMIT = 4
     """The maximum current limit for the TIC 36v4.
-    
+
     .. note::
-    
+
         It is technically possible to change this, both for the hardware and
         in this library, so be sure to make sure they match.
-        
+
     """
 
     def convert_current_value(self, current: float) -> int:
@@ -42,7 +34,11 @@ class TIC36v4_I2C(TicMotorI2C):
         to nearest acceptable value
         """
 
-        acceptable_values = [value for value in range(128) if current >= (value/127)*self.MAX_CURRENT_LIMIT]
+        acceptable_values = [
+            value
+            for value in range(128)
+            if current >= (value / 127) * self.MAX_CURRENT_LIMIT
+        ]
         return max(acceptable_values)
 
     def convert_current_enum(self, enum_value: int) -> float:
@@ -50,4 +46,4 @@ class TIC36v4_I2C(TicMotorI2C):
         current limit
         """
 
-        return enum_value * (enum_value/127) * self.MAX_CURRENT_LIMIT
+        return enum_value * (enum_value / 127) * self.MAX_CURRENT_LIMIT
